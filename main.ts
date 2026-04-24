13ae87d6e70396100db39684163fede1862acb29
Brandon-Tsang <brandon.tsang@stu.ocsb.ca>
2026-04-24 13:08:19 -0400

    receives radio messages

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Brandon
+ * Created on: Apr 2026
+ * This program recieves radio messages
 */
 
-basic.showString('Hello, World!')
+// setup
+radio.setGroup(153)
+basic.showIcon(IconNames.Happy)
+
+// receive radio message
+radio.onReceivedString(function (receivedString) {
+    // display radio messages
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+})
