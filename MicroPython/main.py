4d4172e02a86aeac5f89bc34c048be5a7dc7d5b2
Brandon-Tsang <brandon.tsang@stu.ocsb.ca>
2026-04-24 17:30:56 +0000

    sneds a radio message

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,81 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Brandon
+Created on: Apr 2026
+This module will send radio messages.
 """
 
 from microbit import *
+import radio
 
-display.scroll("Hello, World!")
+class HCSR04:
+    """
+    This class abstracts out the functionality of the HC-SR04 and
+    returns distance in cm
+    Trigger: pin 1
+    Echo: pin 2
+    Serial clock: pin 13
+    """
+
+    def __init__(self, trigger_pin=pin1, echo_pin=pin2, sclk_pin=pin13):
+        self.trigger_pin = trigger_pin
+        self.echo_pin = echo_pin
+        self.sclk_pin = sclk_pin
+
+        spi.init(
+            baudrate=125000,
+            sclk=self.sclk_pin,
+            mosi=self.trigger_pin,
+            miso=self.echo_pin,
+        )
+
+    def get_distance_cm(self) -> int:
+        pre = 0
+        post = 0
+        length = 500
+        response = bytearray(length)
+        response[0] = 0xFF
+        spi.write_readinto(response, response)
+
+        # find first non zero value
+        index_start = -1
+        for idx in range(length):
+            if response[idx]:
+                index_start = idx
+                break
+        if index_start == -1:
+            return -1
+
+        # find first zero value after ping
+        index_end = -1
+        for idx in range(index_start + 1, length):
+            if response[idx] == 0:
+                index_end = idx
+                break
+        if index_end < 0:
+            return -1
+
+        # count bits
+        if index_start > 0:
+            pre = bin(response[index_start]).count("1")
+        if index_end >= 0:
+            post = bin(response[index_end]).count("1")
+
+        return round(((pre + (index_end - index_start) * 8 + post) * 8 * 0.0344) * 0.5)
+
+
+# create sonar instance
+sonar = HCSR04(trigger_pin=pin1, echo_pin=pin2, sclk_pin=pin13)
+
+# setup
+radio.on()
+radio.config(group=153)
+display.show(Image.HAPPY)
+
+# main loop
+while True:
+    if button_a.was_pressed():
+        while True:
+            display.show(Image.TRIANGLE)
+            distance = sonar.get_distance_cm()
+            if distance < 10 and distance > -1:
+                radio.send("Too close")
